eec7e9fdda6c1d76b0a06c7c3390b4c9cf60a0d9
dr386 <dr386.student@gmail.com>
2023-04-03 19:44:03 -0700

    create bookList and bookShow components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
 import "./index.css";
+import BookList from "./components/BookList";
 
 function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    setBooks([...books, title]);
+    const updateBooks = [
+      ...books,
+      {
+        id: Math.round(Math.random() * 9999),
+        title,
+      },
+    ];
+
+    setBooks(updateBooks);
   };
 
-  console.log(books);
-
   return (
-    <div>
+    <div className="app">
+      <BookList books={books} />
       <BookCreate onCreate={createBook} />
     </div>
   );
